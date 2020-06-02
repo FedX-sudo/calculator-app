a81660fdbd609e79006f546ebf6a7987326d14be
FedX-sudo <66258975+FedX-sudo@users.noreply.github.com>
2020-06-02 07:02:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,71 @@
-# Program to create a calculator 
-	
-# Program to Show how to create a switch 
-# import kivy module	 
-import kivy 
-	
-# base Class of your App inherits from the App class.	 
-# app:always refers to the instance of your application 
-from kivy.app import App 
-	
-# this restrict the kivy version i.e 
-# below this kivy version you cannot 
-# use the app or software 
-kivy.require('1.9.0') 
+# Program to Show how to use textinput
+# (UX widget) in kivy using .kv file
 
-# for making multiple bttons to arranging 
-# them we are using this 
-from kivy.uix.gridlayout import GridLayout 
+# import kivy module
+import kivy
 
-# for the size of window 
-from kivy.config import Config 
+# base Class of your App inherits from the App class.
+# app:always refers to the instance of your application
+from kivy.app import App
+from kivy.uix.widget import Widget
 
-# Setting size to resizable 
-Config.set('graphics', 'resizable', 1) 
-## Config.set('graphics', 'width', '400') 
-## Config.set('graphics', 'height', '400') 
+# this restrict the kivy version i.e
+# below this kivy version you cannot
+# use the app or software
+kivy.require('1.9.0')
 
 
-# Creating Layout class 
-class CalcGridLayout(GridLayout): 
+# Widgets are elements
+# of a graphical user interface
+# that form part of the User Experience.
 
-	# Function called when equals is pressed 
-	def calculate(self, calculation): 
-		if calculation: 
-			try: 
-				# Solve formula and display it in entry 
-				# which is pointed at by display 
-				self.display.text = str(eval(calculation)) 
-			except Exception: 
-				self.display.text = "Error"
 
-# Creating App class 
-class CalculatorApp(App): 
+# The TextInput widget provides a
+# box for editable plain text
 
-	def build(self): 
-		return CalcGridLayout() 
+# This layout allows you to set relative coordinates for children.
 
-# creating object and running it 
-calcApp = CalculatorApp() 
-calcApp.run() 
 
+# Create the widget class
+class textinp(Widget):
+    pass
+
+
+# Create the app class
+
+
+class MainApp(App):
+    # Building text input
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.text = None
+
+    def build(self):
+        return textinp()
+
+    # Arranging that what you write will be shown to you
+    # in IDLE
+    def process(self, num, type1):
+        global text
+        if type1 == 1:
+            if self.text == "enter":
+                self.process('1', 4)
+            text = self.root.ids.input.text
+
+        elif type1 == 2:
+            text += num
+        elif type1 == 3:
+            text = ""
+        else:
+            try:
+                # Solve formula and display it in entry
+                # which is pointed at by display
+                print(str(eval(text)))
+            except Exception:
+                print("Error")
+        print(text)
+
+
+# Run the App
+if __name__ == "__main__":
+    MainApp().run()
