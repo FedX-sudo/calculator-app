727f8429584151f2d417e2d3ed9b41cd9a980abc
F G <66258975+FedX-sudo@users.noreply.github.com>
2020-06-06 09:35:15 -0600

    work continues

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import kivy
 # import the python code
 import four_function
 import sci_functoin
+import multiprocessing
 
 kivy.require('1.9.0')
 
+if __name__ == "__main__":
+    four_function.MainApp().run()
 
 def run(num):
-    if __name__ == "__main__":
-        if num == 1:
-            four_function.MainApp().run()
-        if num == 2:
-            sci_functoin.MainApp().run()
-
-
-run(1)
+    if num == 1:
+        four_function.MainApp().run()
+    if num == 2:
+        four_function.MainApp().stop()
+        r = multiprocessing.Process(target=sci_functoin.MainApp.run(self))
+        r.start(self)
