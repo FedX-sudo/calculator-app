b81c36db71d16cbaf44e2eb0de9dbea30f4de687
Fex <X>
2020-06-10 09:16:10 -0600

    create an alpha and a beta version

--- main.py
+++ main.py
@@ -1,18 +1,68 @@
 import kivy
-# import the python code
-import four_function
-import sci_functoin
-import multiprocessing
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.config import Config
 
 kivy.require('1.9.0')
 
-if __name__ == "__main__":
-    four_function.MainApp().run()
+Config.set('graphics', 'width', '300')
+Config.set('graphics', 'height', '375')
+Config.set('graphics', 'resizable', False)
 
-def run(num):
-    if num == 1:
-        four_function.MainApp().run()
-    if num == 2:
-        four_function.MainApp().stop()
-        r = multiprocessing.Process(target=sci_functoin.MainApp.run(self))
-        r.start(self)
+
+class textinp(Widget):
+    pass
+
+
+def entry():
+    return text_inp
+
+
+# Building text input
+
+
+class MainApp(App):
+    global text_inp
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.text = None
+
+    def build(self):
+        return textinp()
+
+    @property
+    def entry(self):
+        return text_inp
+
+    def remove(self, rmtype):
+        global text_inp
+        if rmtype == 1:
+            text_inp = text_inp[:-1]
+        if rmtype == 2:
+            text_inp = ''
+
+
+    def process(self, num, type1):
+        global text_inp
+        if type1 == 1:
+            if self.text == "return":
+                self.process('1', 4)
+
+        elif type1 == 2:
+            text_inp += num
+        else:
+            try:
+                # Solve formula and display it in entry
+                # which is pointed at by display
+                return str(eval(text_inp))
+            except Exception:
+                return "Error"
+        print(text_inp)
+
+
+text_inp = ''
+# Run the App
+if __name__ == "__main__":
+    MainApp().run()
